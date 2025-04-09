8d25ec972ec94f9003a2bf6aad6e85735772badf
Steinn Eldjárn Sigurðarson <steinnes@gmail.com>
2025-04-09 23:51:31 +0000

    Remove unused colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,6 @@ export default {
     "./src/*.{js,ts,jsx,tsx}", // Make sure this covers your component files
   ],
   theme: {
-    extend: {
-      // Add custom theme settings here (like colors)
-      colors: {
-         'input-gray': '#dddddd',
-         'input-gray-greenish': '#dbe0db',
-      },
-    },
   },
   plugins: [],
 }
